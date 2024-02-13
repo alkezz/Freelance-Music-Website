566020951104137499e6f638d3ca8fa1c6c94fc6
Ali <a.k.ezzeddine@gmail.com>
2024-02-13 17:30:19 -0500

    finished with first part of new page, touching up

--- frontend/src/components/DonationPage/Palestine.js
+++ frontend/src/components/DonationPage/Palestine.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 import logo from "../../assets/aliPictureLogo.png"
+import palestine from "../../assets/newpalestine.png"
 import "./Palestine.css";
 
 export default function Palestine() {
+    document.title = 'YWK Ali | Fundraiser';
     return (
         <>
             <div className="top-navbar-donations">
@@ -16,7 +18,14 @@ export default function Palestine() {
                 </div>
                 {/* <Link className="donate-page-music-link" to="/">Home</Link> */}
             </div>
-            <h1>Fundraiser</h1>
+            <div className="main-page-container">
+                <h1>Fundraiser</h1>
+                <button id='main-donate-button'>Donate</button>
+                <h1>$0 / $1,000</h1>
+                <img alt="palestine map" src={palestine} />
+                <p style={{ fontSize: '28px', color: '#f5deb3', textAlign: 'center', padding: '10px' }}>The Palestinians are suffering from <span style={{ textDecoration: 'underline', fontSize: '32px', color: 'black' }}>GENOCIDE</span> at the hands of the Zionist entity.</p>
+                <p style={{ fontSize: '28px', color: '#f5deb3', textAlign: 'center' }}>No ifs, ands, or buts, we must help.</p>
+            </div>
         </>
     )
 }
