c278645b5e4477a626d91cc41b3c0cbc8f6964f5
Ali <a.k.ezzeddine@gmail.com>
2024-02-14 17:48:58 -0500

    adding conditional rendering

--- frontend/src/components/DonationPage/Palestine.js
+++ frontend/src/components/DonationPage/Palestine.js
@@ -10,6 +10,18 @@ export default function Palestine() {
     //Fundraiser
     document.title = 'YWK Ali | Fundraiser';
     document.body.style.backgroundColor = '#b11e1e'
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const next = () => {
+        setCurrentSlide(prevSlide => prevSlide + 1);
+    };
+    const prev = () => {
+        setCurrentSlide(prevSlide => prevSlide - 1);
+    };
+    const updateCurrentSlide = (index) => {
+        if (currentSlide !== index) {
+            setCurrentSlide(index);
+        }
+    };
     return (
         <>
             <div className="top-navbar-donations">
@@ -22,44 +34,70 @@ export default function Palestine() {
                 </div>
                 {/* <Link className="donate-page-music-link" to="/">Home</Link> */}
             </div>
-            <div className="main-page-container">
-                <h1>Fundraiser</h1>
-                <button id='main-donate-button'>Donate</button>
-                <h1>$0 / $1,000</h1>
-                <img alt="palestine map" src={palestine} />
-                <p style={{ fontSize: '28px', color: '#f5deb3', textAlign: 'center', padding: '10px' }}>The Palestinians are suffering from <span style={{ textDecoration: 'underline', fontSize: '32px', color: 'black' }}>GENOCIDE</span> at the hands of the Zionist entity.</p>
-                <p style={{ fontSize: '28px', color: '#f5deb3', textAlign: 'center' }}>No ifs, ands, or buts, we <span style={{ textDecoration: 'underline', fontSize: '32px', color: 'black' }}>MUST</span> help.</p>
-            </div>
-            <div className="carousel-container">
-                <h1 style={{ textDecoration: 'underline' }}>Gallery</h1>
-                <h2>ALL PROCEEDS WILL GO TO PALESTINE!</h2>
-                <br />
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <Carousel className="crsl" infiniteLoop centerMode centerSlidePercentage={85}>
-                        <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=5' />
-                        <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=4' />
-                        <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=3' />
-                        <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=2' />
-                        <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=1' />
-                    </Carousel>
-                    <div className="about-product">
-                        <h2>Product Name</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
-                        <p style={{ fontSize: '30px' }}>$10</p>
-                        <button id='main-donate-button'>I Want This</button>
+            <div style={{ overflowX: 'hidden' }}>
+                <div className="main-page-container">
+                    <h1>Fundraiser</h1>
+                    <button id='main-donate-button'>Donate</button>
+                    <h1>$0 / $1,000</h1>
+                    <img alt="palestine map" src={palestine} />
+                    <p style={{ fontSize: '28px', color: '#f5deb3', textAlign: 'center', padding: '10px' }}>The Palestinians are suffering from <span style={{ textDecoration: 'underline', fontSize: '32px', color: 'black' }}>GENOCIDE</span> at the hands of the Zionist entity.</p>
+                    <p style={{ fontSize: '28px', color: '#f5deb3', textAlign: 'center' }}>No ifs, ands, or buts, we <span style={{ textDecoration: 'underline', fontSize: '32px', color: 'black' }}>MUST</span> help.</p>
+                </div>
+                <div className="carousel-container">
+                    <h1 style={{ textDecoration: 'underline' }}>Gallery</h1>
+                    <h2>ALL PROCEEDS WILL GO TO PALESTINE!</h2>
+                    <br />
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <button onClick={prev}>
+                            Prev
+                        </button>
+                        <button onClick={next}>
+                            Next
+                        </button>
+                        <Carousel onChange={updateCurrentSlide} selectedItem={currentSlide} className="crsl" infiniteLoop centerMode centerSlidePercentage={85}>
+                            <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=5' />
+                            <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=4' />
+                            <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=3' />
+                            <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=2' />
+                            <img style={{ padding: '15px' }} src='https://picsum.photos/300?random=1' />
+                        </Carousel>
+                        <div className="about-product">
+                            <h2>Product Name</h2>
+                            {/* {currentSlide === 0 && (
+                                <>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
+                                    <p style={{ fontSize: '30px' }}>$10</p>
+                                </>
+                            )}
+                            {currentSlide === 1 && (
+                                <>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
+                                    <p style={{ fontSize: '30px' }}>$25</p>
+                                </>
+                            )}
+                            {currentSlide === 2 && (
+                                <>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo ullamcorper a lacus vestibulum. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor. Platea dictumst quisque sagittis purus.</p>
+                                    <p style={{ fontSize: '30px' }}>$50</p>
+                                </>
+                            )} */}
+                            <button id='main-donate-button'>I Want This</button>
+                        </div>
                     </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <p style={{ fontSize: '32px', marginBottom: '-10px' }}>I Don't Care</p>
+                        <p style={{ fontSize: '32px', marginBottom: '-10px' }}>I just wanna donate!</p>
+                        <p style={{ fontSize: '32px' }}>No worries! Thank you so much!</p>
+                        <button id='main-donate-button'>DONATE</button>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
                 </div>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <p style={{ fontSize: '32px', marginBottom: '-10px' }}>I Don't Care</p>
-                    <p style={{ fontSize: '32px', marginBottom: '-10px' }}>I just wanna donate!</p>
-                    <p style={{ fontSize: '32px' }}>No worries! Thank you so much!</p>
-                    <button id='main-donate-button'>DONATE</button>
-                </div>
-                <br />
-                <br />
-                <br />
-            </div>
+            </div >
         </>
     )
 }
