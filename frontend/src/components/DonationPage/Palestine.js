92442f89f8f231a973e4239566204157c7fb48d2
Ali <a.k.ezzeddine@gmail.com>
2024-03-09 22:11:16 -0500

    almost done

--- frontend/src/components/DonationPage/Palestine.js
+++ frontend/src/components/DonationPage/Palestine.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom"
 import logo from "../../assets/aliPictureLogo.png"
 import palestine from "../../assets/newpalestine.png"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import cashapp from "../../assets/cashapp.png"
+import venmo from "../../assets/venmo.png"
+import pcrf from "../../assets/pcrf.png"
 import "./Palestine.css";
 
 export default function Palestine() {
@@ -34,47 +36,33 @@ export default function Palestine() {
                 </div>
                 {/* <Link className="donate-page-music-link" to="/">Home</Link> */}
             </div>
+            <br />
             <div style={{ overflowX: 'hidden' }}>
                 <div className="main-page-container">
                     <h1 style={{ marginBottom: '-20px' }}>Fundraiser</h1>
                     <h1 style={{ marginBottom: '-20px' }}>For</h1>
-                    <h1 style={{}}>Falasteen</h1>
-                    <button onClick={(e) => document.getElementById('scroll-to').scrollIntoView({ behavior: 'smooth' })} id='main-donate-button'>Donate</button>
+                    <h1>Falasteen</h1>
+                    <img src={pcrf} id='pcrf' />
+                    <a href='https://pcrf1.app.neoncrm.com/YouWillKnowAli' target='_blank' referrer='noreferrer'>
+                        <button id='main-donate-button'>Donate</button>
+                    </a>
+                    <div>
+                        <a href='https://cash.app/$aliezzobezzo' target='_blank'>
+                            <img id='small-donate-button' src={cashapp} />
+                        </a>
+                        <a href='https://account.venmo.com/u/youwillknowali' target='_blank'>
+                            <img id='small-donate-button' src={venmo} />
+                        </a>
+                    </div>
                     <h1 style={{}}>$0 / $1,000</h1>
                     <img alt="palestine map" src={palestine} />
                 </div>
                 <br />
                 <div className="carousel-container">
-                    <h2 style={{ fontSize: '28px', color: '#ebcca4', textAlign: 'center', padding: '10px', marginBottom: '-30px' }}>Palestinians are suffering from <span style={{ textDecoration: 'underline', fontSize: '32px', color: 'black', textShadow: '0px 0px 0px' }}>GENOCIDE</span>.</h2>
-                    <h2 style={{ fontSize: '28px', color: '#ebcca4', textAlign: 'center', marginBottom: '-10px' }}>By donating, you are sending direct aid to those who need it.</h2>
-                    <h2 style={{ textAlign: 'center', color: '#ebcca4', fontSize: '28px' }}>ALL PROCEEDS WILL GO TO PALESTINE</h2>
-                    <br />
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '-20px' }}>
-                        {/* <button onClick={prev}>
-                            Prev
-                        </button>
-                        <button onClick={next}>
-                            Next
-                        </button> */}
-                        {/* <Carousel onChange={updateCurrentSlide} selectedItem={currentSlide} className="crsl" infiniteLoop centerMode centerSlidePercentage={65}>
-                            <img alt='random picture' style={{ padding: '15px' }} src='https://picsum.photos/300?random=5' />
-                            <img alt='random picture' style={{ padding: '15px' }} src='https://picsum.photos/300?random=4' />
-                            <img alt='random picture' style={{ padding: '15px' }} src='https://picsum.photos/300?random=3' />
-                            <img alt='random picture' style={{ padding: '15px' }} src='https://picsum.photos/300?random=2' />
-                            <img alt='random picture' style={{ padding: '15px' }} src='https://picsum.photos/300?random=1' />
-                        </Carousel>
-                        <div className="about-product">
-                            <h2 style={{ marginTop: '-40px' }}>Product Name</h2>
-                            <p style={{ fontSize: '30px', display: 'flex', justifyContent: 'center', marginTop: '-10px' }}>$10</p>
-                            <h2 style={{ fontSize: '20px', marginTop: '-10px' }}>Made by: John Doe</h2>
-                            <button className='buy-button'>I Want This</button>
-                        </div> */}
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: '#f5deb3', textAlign: 'center', marginTop: '-50px' }}>
-                        {/* <h2 style={{ fontSize: '32px', marginTop: '-10px', fontSize: '18px' }} >Thank you so much!</h2>
-                        <button className='donate-button'>DONATE</button> */}
-                    </div>
-                    <br />
+                    {/* <h2 style={{ fontSize: '28px', color: '#ebcca4', textAlign: 'center', padding: '10px', marginBottom: '-30px' }}>Palestinians are suffering from <span style={{ textDecoration: 'underline', fontSize: '32px', color: 'black', textShadow: '0px 0px 0px' }}>GENOCIDE</span>.</h2>
+                    <h2 style={{ fontSize: '28px', color: '#ebcca4', textAlign: 'center', marginBottom: '-10px' }}>By donating, you are sending direct aid to those who need it.</h2> */}
+                    <h2 style={{ textAlign: 'center', color: '#ebcca4', fontSize: '28px' }}>ALL PROCEEDS WILL GO TO PALESTINE THROUGH PCRF</h2>
+                    <h2 style={{ textAlign: 'center', color: '#ebcca4', fontSize: '28px', marginTop: '-20px' }}>Transactions done through Cashapp and Venmo will be donated in your name.</h2>
                     <br />
                     <br />
                 </div>
